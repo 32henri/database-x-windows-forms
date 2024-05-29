447bad6f7cca999ba996d8e1704ac96b29b7c28d
Your Name <you@example.com>
2024-05-29 15:02:27 +0300

    need erd

---
+++ WinFormsApp1/WinFormsApp1/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace WinFormsApp1;
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ObservableCollectionListSource<Product> Products { get; } = new();
+}
